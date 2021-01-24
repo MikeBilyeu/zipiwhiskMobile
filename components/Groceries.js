cf42debb5278500eab6c1c9f8bd1fcffdf2d0522
Mike Bilyeu <mikebilyeu512@gmail.com>
2021-01-24 01:10:50 -0500

    Make list editable

--- components/Groceries.js
+++ components/Groceries.js
@@ -11,6 +11,7 @@ import {
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
   Keyboard,
+  Touchable,
 } from "react-native";
 import "react-native-get-random-values";
 import { v4 as uuidv4 } from "uuid";
@@ -28,22 +29,38 @@ function Header() {
     </View>
   );
 }
+
+const renderList = (list, setList) =>
+  list.map((i, index) => (
+    <TouchableOpacity activeOpacity={0.8} key={index} style={{ width: "100%" }}>
+      <View style={styles.ingredientContainer}>
+        <View style={styles.ingredientWrapper}>
+          <TextInput
+            key={i.id}
+            style={styles.ingredientName}
+            onChangeText={(text) => {
+              let newList = [...list];
+              newList[index].ingredient = text;
+              setList(newList);
+            }}
+            onBlur={() => {
+              !i.ingredient && setList(list.filter((item, i) => i != index));
+            }}
+            value={i.ingredient}
+            returnKeyType="done"
+          />
+          {/* <Text style={styles.ingredientName}>{i.ingredient}</Text>
+      <Text style={styles.ingredientAmount}>{i.amount}</Text> */}
+        </View>
+        <Image source={{ uri: i.image }} style={styles.ingredientImage} />
+      </View>
+    </TouchableOpacity>
+  ));
+
 const Groceries = () => {
   const [inputText, setInputText] = useState("");
   const [list, setList] = useState(groceryData.groceries);
   const inputField = useRef(null);
-
-  const renderList = (list) => {
-    return list.map((i, index) => (
-      <View key={uuidv4()} style={styles.ingredientContainer}>
-        <View style={styles.ingredientWrapper}>
-          <Text style={styles.ingredientName}>{i.ingredient}</Text>
-          <Text style={styles.ingredientAmount}>{i.amount}</Text>
-        </View>
-        <Image source={{ uri: i.image }} style={styles.ingredientImage} />
-      </View>
-    ));
-  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -53,39 +70,43 @@ const Groceries = () => {
         keyboardVerticalOffset={10}
       >
         <Header />
-        <ScrollView style={styles.listContainer}>
-          {renderList(list)}
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <ScrollView style={styles.listContainer}>
+            {renderList(list, setList)}
 
-          <View style={styles.inputContainer}>
-            <TextInput
-              style={styles.inputText}
-              onChangeText={(text) => setInputText(text)}
-              value={inputText}
-              ref={inputField}
-              returnKeyType="done"
-              //blurOnSubmit={false}
-              onBlur={() => {
-                inputText &&
-                  setList(
-                    [
-                      ...list,
-                      {
-                        amount: null,
-                        ingredient: inputText,
-                        image: null,
-                      },
-                    ],
-                    setInputText("")
-                  );
-              }}
-            />
-          </View>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.inputText}
+                onChangeText={(text) => setInputText(text)}
+                value={inputText}
+                ref={inputField}
+                returnKeyType="done"
+                placeholder="Add Item"
+                //blurOnSubmit={false}
+                onBlur={() => {
+                  inputText &&
+                    setList(
+                      [
+                        ...list,
+                        {
+                          id: uuidv4(),
+                          amount: null,
+                          ingredient: inputText,
+                          image: null,
+                        },
+                      ],
+                      setInputText("")
+                    );
+                }}
+              />
+            </View>
 
-          <TouchableOpacity
-            style={styles.addItemBtn}
-            onPress={() => inputField.current.focus()}
-          ></TouchableOpacity>
-        </ScrollView>
+            <TouchableOpacity
+              style={styles.addItemBtn}
+              onPress={() => inputField.current.focus()}
+            ></TouchableOpacity>
+          </ScrollView>
+        </TouchableWithoutFeedback>
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
@@ -126,24 +147,29 @@ const styles = StyleSheet.create({
 
   ingredientContainer: {
     flexDirection: "row",
-    alignItems: "center",
+    alignItems: "flex-start",
     justifyContent: "space-between",
-    paddingHorizontal: 15,
-    paddingVertical: 15,
+    paddingRight: 15,
+    height: 85,
+  },
+  ingredientWrapper: {
+    justifyContent: "space-between",
+    flex: 0.9,
+    height: 85,
   },
   ingredientImage: {
     width: 45,
     height: 45,
     borderRadius: 10,
+    alignSelf: "center",
   },
-  ingredientWrapper: {
-    justifyContent: "space-between",
-    flex: 1,
-  },
+
   ingredientName: {
     fontSize: 18,
     fontFamily: "AvenirNextBold",
     color: "#313131",
+    paddingLeft: 15,
+    height: 85,
   },
   ingredientAmount: {
     marginTop: 5,
@@ -173,23 +199,27 @@ export default Groceries;
 const groceryData = {
   groceries: [
     {
+      id: uuidv4(),
       amount: "¼ cup",
       ingredient: "All-purpose flour",
       image:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrmaF1M32DN3r7ciBu7pGxbv_jGrBq-6vlfc8F_JZlyJY8lfcFK59_LaCp0Ac_p52crRxruIi5&usqp=CAc",
     },
     {
+      id: uuidv4(),
       amount: "1 Tbsp + 2 tsp",
       ingredient: "Vegatable oil ",
       image:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3WIIweap2Y7yXbnNS8qTVs5kI_HRDX9WlpzYTy8M12YBFwR20vfVL2zpRwc4&usqp=CAc",
     },
     {
+      id: uuidv4(),
       amount: "1 cup",
       ingredient: "Red wine",
       image: "https://images.heb.com/is/image/HEBGrocery/000538201",
     },
     {
+      id: uuidv4(),
       amount: "2",
       ingredient: "Bay leaves",
       image:
