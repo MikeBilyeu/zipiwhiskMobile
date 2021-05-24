9cee49127c469c60e488be91ef15e64c3e142fd9
Mike Bilyeu <mikebilyeu512@gmail.com>
2021-05-24 17:55:45 -0400

    Refactor destructured props

--- components/CreateRecipeScreen/Input.js
+++ components/CreateRecipeScreen/Input.js
@@ -1,37 +1,24 @@
 import React, { useState } from "react";
 import { StyleSheet, View, TextInput, Text } from "react-native";
 
-const Input = ({
-  name,
-  info,
-  value,
-  setValue,
-  placeholder,
-  multiline,
-  keyboardType = "default",
-  returnKeyType = "next",
-}) => {
+const Input = (props) => {
   const [height, setHeight] = useState(0);
   return (
     <View style={[styles.container, { height: Math.max(85, height + 55) }]}>
       <View style={styles.nameWrapper}>
-        <Text style={styles.nameText}>{name}</Text>
-        <Text style={styles.infoText}>{info}</Text>
+        <Text style={styles.nameText}>{props.name}</Text>
+        <Text style={styles.infoText}>{props.info}</Text>
       </View>
 
       <TextInput
-        style={[styles.textInput, multiline && { paddingTop: 35 }]}
-        defaultValue={value}
-        onChangeText={(text) => setValue(text)}
-        placeholder={placeholder}
+        style={[styles.textInput, props.multiline && { paddingTop: 35 }]}
+        onChangeText={(text) => props.setValue(text)}
         placeholderTextColor="#E2E2E2"
-        multiline={multiline}
         scrollEnabled={false}
-        keyboardType={keyboardType}
-        returnKeyType={returnKeyType}
         onContentSizeChange={(event) =>
           setHeight(event.nativeEvent.contentSize.height)
         }
+        {...props}
       />
     </View>
   );
