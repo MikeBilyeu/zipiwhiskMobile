dba6452aa573c112cbc71e7ef2fa7b0431a13d47
Mike Bilyeu <mikebilyeu512@gmail.com>
2021-06-18 16:25:53 -0400

    Replace require with import

--- components/UserImageBtn.js
+++ components/UserImageBtn.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { TouchableOpacity, Image } from "react-native";
+import defaultImage from "../assets/userImage.png";
 
 const UserImageBtn = (props) => (
   <TouchableOpacity onPress={props.handleImagePress} activeOpacity={0.5}>
     <Image
       style={[{ borderRadius: 50 }, props.styles]}
-      source={
-        props.uri ? { uri: props.uri } : require("../assets/userImage.png")
-      }
+      source={props.uri ? { uri: props.uri } : defaultImage}
     />
   </TouchableOpacity>
 );
