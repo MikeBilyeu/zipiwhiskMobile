df6b6666b1e4c6b7f2c089bb213754a7a6c35ba1
Mike Bilyeu <mikebilyeu512@gmail.com>
2021-10-07 20:47:25 -0400

    Fix gradient

--- components/Header/index.js
+++ components/Header/index.js
@@ -4,21 +4,20 @@ import { LinearGradient } from "expo-linear-gradient";
 
 const Header = (props) => {
   return (
-    <SafeAreaView style={{ zIndex: 2 }}>
-      <LinearGradient
-        colors={["rgba(255,255,255,0)", "rgba(255,255,255,1)"]}
-        start={[0, 1]}
-        end={[0, 0.9]}
-        style={styles.gradientWrapper}
-      >
-        {props.children}
-      </LinearGradient>
-    </SafeAreaView>
+    <LinearGradient
+      colors={["rgba(255,255,255,0)", "rgba(255,255,255,1)"]}
+      start={[0, 1]}
+      end={[0, 0.8]}
+      style={styles.gradientWrapper}
+    >
+      {props.children}
+    </LinearGradient>
   );
 };
 
 const styles = StyleSheet.create({
   gradientWrapper: {
+    zIndex: 2,
     width: "100%",
     alignItems: "center",
     justifyContent: "space-between",
