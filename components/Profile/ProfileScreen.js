2e06cf7d5b4d97054b1436c18ed90e247297fa67
Mike Bilyeu <mikebilyeu512@gmail.com>
2022-04-28 20:24:27 -0400

    Add useEffect for focus

--- components/Profile/ProfileScreen.js
+++ components/Profile/ProfileScreen.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet } from "react-native";
 import SafeAreaView from "react-native-safe-area-view";
-
+import { useIsFocused } from "@react-navigation/native";
 import ProfileHeader from "./Header/ProfileHeader";
 import ProfileTab from "./ProfileTab";
 
-const ProfileScreen = () => (
-  <SafeAreaView style={styles.container} forceInset={{ top: "always" }}>
-    <ProfileHeader />
-    <ProfileTab />
-  </SafeAreaView>
-);
+const ProfileScreen = (props) => {
+  const isFocused = useIsFocused();
+  useEffect(() => {
+    if (isFocused) {
+      console.log("Screen Has Focus");
+    }
+  }, [isFocused]);
+  return (
+    <SafeAreaView style={styles.container} forceInset={{ top: "always" }}>
+      <ProfileHeader />
+      <ProfileTab />
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -18,5 +26,4 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFF",
   },
 });
-
 export default ProfileScreen;
